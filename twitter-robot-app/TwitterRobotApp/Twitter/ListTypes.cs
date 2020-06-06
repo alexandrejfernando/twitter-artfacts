0e87e1ec85a38c24ad99eff4e0b33548a97a9b39
Alexandre José Fernando <alexandrej@ciandt.com>
2020-06-06 18:13:51 -0300

    inicial

---
+++ twitter-robot-app/TwitterRobotApp/Twitter/ListTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitterRobotApp.Twitter
+{
+    public static class ListTypes
+    {
+        public enum TwitterApiList 
+        {
+            Friends = 0,
+            Followers = 1,
+            Friendship_Unfollow = 2,
+            Friends_List = 3
+        }
+    }
+}
