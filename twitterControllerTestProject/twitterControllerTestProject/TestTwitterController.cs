509c03ca6b5aada47ae9049bc274ddcc38218037
Alexandre José Fernando <alexandrej@ciandt.com>
2020-06-06 18:48:07 -0300

    test project + yml ci

---
+++ twitterControllerTestProject/twitterControllerTestProject/TestTwitterController.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace twitterControllerTestProject
+{
+    public class TestTwitterController
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+
+        }
+    }
+}
